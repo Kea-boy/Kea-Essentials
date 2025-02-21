5926c69da8e637eb4224bb141adf0f2809932400
Kea-boy <keaboymodiko@gmail.com>
2025-02-21 23:49:37 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'Kea', // Your name
-  ownerNumber: '27684612536', //Your number
+  ownerNumber: '27671820721', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
